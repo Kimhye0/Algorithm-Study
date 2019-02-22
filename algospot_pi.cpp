2ddd68c3fe6a38eb6cdce31b2ebfa6269946bb4d
Kimhye0 <akrnrh3737@naver.com>
2019-02-23 01:17:17 +0900

    algospot_pi
    원주율외우기

---
+++ algospot_pi.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int cache[10000];
+int INF = 987654321;
+string N;
+
+int classify(int a, int b) {
+	string M = N.substr(a, b - a + 1);
+	if (M == string(M.size(), M[0]))
+		return 1;
+	
+	bool isProgress = true;
+	for (int i = 0; i < M.size() - 1; i++) {
+		if (M[i + 1] - M[i] != M[1] - M[0])
+			isProgress = false;
+	}
+	if (isProgress && abs(M[1] - M[0]) == 1)
+		return 2;
+
+	bool isRotate = true;
+	for (int i = 0; i < M.size(); i++) {
+		if (M[i] != M[i % 2])
+			isRotate = false;
+	}
+	if (isRotate) return 4;
+	if (isProgress) return 5;
+	
+	return 10;
+}
+
+int memo(int i) {
+	//base case
+	if (i == N.size()) return 0;
+	//memoization
+	int& ret = cache[i];
+	if (ret != -1) return ret;
+	ret = INF;
+	for (int len = 3; len <= 5; len++) {
+		if (i + len <= N.size()) {
+			ret = min(ret, memo(i + len) + classify(i, i + len - 1));
+		}
+	}
+	return ret;
+}
+
+int main() {
+	int c;
+	cin >> c;
+	while (c--) {
+		memset(cache, -1, sizeof(cache));
+		cin >> N;
+		cout << memo(0) << endl;
+	}
+
+	return 0;
+}
