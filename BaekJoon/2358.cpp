4939be4e949c469d1eb08c16573d6f88997f8277
Kimhye0 <akrnrh3737@naver.com>
2019-03-03 00:49:59 +0900

    2358:평행선
    O(nlogn)

--- BaekJoon/2358.cpp
+++ BaekJoon/2358.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
-#include <vector>
+#include <map>
 #include <utility>
 
 using namespace std;
@@ -10,19 +11,33 @@ int n;
 int main() {
 	int cnt = 0;
 	cin >> n;
-	vector< pair<int, int> > cord(n);
+	/*vector< pair<int, int> > cord(n);*/
+	map<int, int> X, Y;
 	for (int i = 0; i < n; i++) {
-		cin >> cord[i].first >> cord[i].second;
+		/*cin >> cord[i].first >> cord[i].second;*/
+		int x, y;
+		cin >> x >> y;
+		X[x]++;
+		Y[y]++;
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = i+1; j < n; j++) {
-			if (cord[i].first == cord[j].first) {
-				cnt++;
-			}
-			if (cord[i].second == cord[j].second) {
-				cnt++;
-			}
-		}
+	/*Time Complexity : O(n^)*/
+	//for (int i = 0; i < n; i++) {
+	//	for (int j = i+1; j < n; j++) {
+	//		if (cord[i].first == cord[j].first) {
+	//			cnt++;
+	//		}
+	//		if (cord[i].second == cord[j].second) {
+	//			cnt++;
+	//		}
+	//	}
+	//}
+
+	/*Time Complexity : O(nlogn)*/
+	for (map<int, int>::iterator it = X.begin(); it != X.end(); it++) {
+		if (it->second >= 2) cnt++;
+	}
+	for (map<int, int>::iterator it = Y.begin(); it != Y.end(); it++) {
+		if (it->second >= 2) cnt++;
 	}
 	cout << cnt << endl;
 	return 0;
